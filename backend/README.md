dbad32caf7a54af3593c82ecb00e14f303e1c573
umeneri <evitechpartner@gmail.com>
2023-06-22 02:14:52 +0900

    rename

--- backend/README.md
+++ backend/README.md
@@ -4,3 +4,7 @@ docker volume create --name=blog-mysql-data
 docker-compose up -d
 
 
+# 参考
+
+https://zenn.dev/rince/articles/50a66241d04f0b
+
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,13 +6,23 @@ import { PostsResolver } from './resolvers/posts.resolver';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersResolver } from './resolvers/users.resolver';
+import { PostsModule } from './components/posts/posts.module';
+import * as path from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import configuration from '../config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: process.cwd() + '/src/schema.gql',
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: path.join(process.cwd(), '/src/schema.gql'),
     }),
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService, UsersResolver, PostsResolver],
