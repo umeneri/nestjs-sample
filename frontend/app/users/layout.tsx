dcd73870da236f9a5573ab409433471ca0b08c0a
umeneri <evitechpartner@gmail.com>
2023-06-22 13:31:29 +0900

    prettier

---
+++ frontend/app/users/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <section>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      {children}
+    </section>
+  )
+}
