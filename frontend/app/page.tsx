e8b1fa3da65e67b273d325db2f4c0355db5eff28
umeneri <evitechpartner@gmail.com>
2023-06-22 13:08:54 +0900

    frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,12 @@
-import { GetServerSideProps } from "next";
 import Image from 'next/image'
 
-type Props = {
-  title: string
-};
-
-export default function Home(props: Props) {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {props.title}
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -115,11 +111,3 @@ export default function Home(props: Props) {
     </main>
   )
 }
-
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  return {
-    props: {
-      title: "Hello, GraphQL!",
-    },
-  };
-};
